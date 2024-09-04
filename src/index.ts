cf8e1ed4aec0b14ada52ceb873f1acdcf0117f9e
Biya Paul <bpsmartdesign@hotmail.com>
2024-09-04 10:14:40 +0100

    core: main file

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as Button } from "./components/base/btn";
+export { default as Text } from "./components/base/typographie/txt";
+export { default as Title } from "./components/base/typographie/title";
